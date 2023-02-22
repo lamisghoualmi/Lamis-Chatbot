404dc240cb4cd205a5227687c9d4c6bae38341fe
lamis ghoualmi <56949028+lamisghoualmi@users.noreply.github.com>
2023-02-22 17:38:10 -0500

    Update ChatbotMain.py

--- ChatbotMain.py
+++ ChatbotMain.py
@@ -57,7 +57,7 @@ if 'past' not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("You [enter your message here]: "," Hello Mr AI how was your day today? ", key="input")
+    input_text = st.text_input("You [enter your message here]: "," Hello Mr AI, how was your day? ", key="input")
     return input_text 
 
 
